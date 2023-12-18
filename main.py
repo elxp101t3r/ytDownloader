b15a882b7677acdf754946fbc5f830cfa647310b
elxp101t3r <eliotx10101@outlook.com>
2023-12-18 21:02:12 +0200

    Change the bg in something sexy

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+from kivy.core.window import Window
+
+
+Window.size=(500,600)
 
 class App(MDApp):
     def build(self):
-        layout = MDRelativeLayout()
+        layout = MDRelativeLayout(md_bg_color = [248/255, 200/255, 220/255])
+        
+        
         return layout
 
 if __name__ == '__main__':
