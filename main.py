a22a2cda4ad3982a81739b7c35285f3b56f30b8c
elxp101t3r <eliotx10101@outlook.com>
2023-12-18 21:34:23 +0200

    text field

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ class App(MDApp):
         layout = MDRelativeLayout(md_bg_color = [124/255, 200/255, 255/255])
         self.img = Image(source='logo.png', size_hint= (.5, .5), pos_hint={'center_x': 0.5, 'center_y': 0.90})
         self.youtubeLink = Label(text='URL', pos_hint={'center_x': 0.5 , 'center_y': .75}, size_hint=(1,1), font_size=20, color=(0,0,0))
+        self.link_input = TextInput(text='', pos_hint={'center_x': 0.5 , 'center_y': 0.65 }, size_hint= (1, None), height=48, font_size=29, foreground_color=(0, 0, 0))
+        
         
         layout.add_widget(self.img)
         layout.add_widget(self.youtubeLink)
-       
+        layout.add_widget(self.link_input)
+        
+
         return layout
 
 if __name__ == '__main__':
