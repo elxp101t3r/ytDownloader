beab09a3f679db69501585540bfaf5eb02c7f854
elxp101t3r <eliotx10101@outlook.com>
2023-12-18 21:24:45 +0200

    title widget for the search bar

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ Window.size=(500,600)
 
 class App(MDApp):
     def build(self):
-        layout = MDRelativeLayout(md_bg_color = [248/255, 200/255, 220/255])
+        layout = MDRelativeLayout(md_bg_color = [124/255, 200/255, 255/255])
         self.img = Image(source='logo.png', size_hint= (.5, .5), pos_hint={'center_x': 0.5, 'center_y': 0.90})
-        layout.add_widget(self.img)
+        self.youtubeLink = Label(text='URL', pos_hint={'center_x': 0.5 , 'center_y': .75}, size_hint=(1,1), font_size=20, color=(0,0,0))
         
+        layout.add_widget(self.img)
+        layout.add_widget(self.youtubeLink)
+       
         return layout
 
 if __name__ == '__main__':
