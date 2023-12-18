e5c8e9b9a6695c2c41e0684391a960ecb569571a
elxp101t3r <eliotx10101@outlook.com>
2023-12-18 21:15:37 +0200

    Add logo

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ Window.size=(500,600)
 class App(MDApp):
     def build(self):
         layout = MDRelativeLayout(md_bg_color = [248/255, 200/255, 220/255])
-        
+        self.img = Image(source='logo.png', size_hint= (.5, .5), pos_hint={'center_x': 0.5, 'center_y': 0.90})
+        layout.add_widget(self.img)
         
         return layout
 
